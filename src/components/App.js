7aa78eb72952ff523172b6ed6614e7506054f3f8
tusharsharma000 <84301919+tusharsharma000@users.noreply.github.com>
2022-04-13 14:43:46 +0530

    String

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
+  let [height, setheight] = useState(320);
+  let [width, setwidth] = useState(320);
+  const handleheightchange = (e) => {
+    setheight(e.target.value);
+  };
+  const handlewidthchange = (e) => {
+    setwidth(e.target.value);
+  };
   return (
     <div id="main">
-
+      <img
+        src="./src/image.jpeg"
+        id="resizable-img"
+        height={height}
+        width={width}
+      />
+      <br />
+      <input
+        type="range"
+        id="height-slider"
+        min={100}
+        max={800}
+        onChange={handleheightchange}
+      />
+      <br />
+      <input
+        type="range"
+        id="width-slider"
+        min={100}
+        max={800}
+        onChange={handlewidthchange}
+      />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
